2816cf8cdab2cab453873429314c15ef007f1028
aaman <aneela.aman.is@gmail.com>
2024-03-26 12:13:49 -0400

    remove comments after game completion

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 // ** Game : Rock Paper Scissors**
 
-// Make a variable getComputerChoice
-// write a function which randomly returns either rock, paper or scissors
-// test this function to confirm it is working
 
 let array = ["rock", "paper", "scissors"];
-
  
 computerSelection = array[Math.floor(Math.random() *array.length)];
-// return (ans);
 
-
-
-// write a function for playerSelection- should be case insensitive
-
-            // function playerSelection() {
-                
-            // }
-
+           
 let ans = prompt("Rock, Paper or Scissors? Make your selection!");
 playerSelection = ans.toLowerCase();
 
 console.log ("You selected " + playerSelection, " Computer selected " + computerSelection);
 
-// write a function that plays a single round of Rock Paper Scissors
-// this function should have two parameters, the playerSelection and computerSelection
-// the function should return a string that declares a winner or tie: "You lose! Paper beats Rock"
-// return the results of this function call, not console.log()
 
 function playRound() {
     if (playerSelection === computerSelection ) {
