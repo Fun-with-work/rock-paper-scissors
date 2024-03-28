d30f6d81e36aac5b57718094a8a87720b659003f
aaman <aneela.aman.is@gmail.com>
2024-03-28 13:08:30 -0400

    remove extra code and change variable names

--- index.js
+++ index.js
@@ -1,96 +1,93 @@
 // ** Game : Rock Paper Scissors**
 
-   
-    let y = 0; 
-    let n = 0;
+
+let winRound = 0;
+let loseRound = 0;
 
 function playGame() {
-   
 
-    for (let count = 0; count<5; count++)  {
-     
+
+    for (let roundCount = 0; roundCount < 5; roundCount++) {
+
 
         let array = ["ROCK", "PAPER", "SCISSORS"];
-        
-    
-        computerSelection = array[Math.floor(Math.random() *array.length)];
-    
-                
-        let ans = prompt("ROCK, PAPER or SCISSORS? Make your selection!");
-        playerSelection = ans.toUpperCase();
-    
-        console.log ("You selected " + playerSelection, " Computer selected " + computerSelection);
-    
-    
+
+        computerSelection = array[Math.floor(Math.random() * array.length)];
+
+        let answer = prompt("ROCK, PAPER or SCISSORS? Enter your selection!");
+        playerSelection = answer.toUpperCase();
+
+        console.log("You selected " + playerSelection, " Computer selected " + computerSelection);
+
+
         function playRound() {
-            if (playerSelection === computerSelection ) {
-                console.log ("It is a tie!");
-                // return playGame();
-            }   
-            
-            else if (playerSelection === "ROCK") {
-                if (computerSelection === "SCISSORS"){
-                    console.log ("You win! ROCK beats SCISSORS")
-                    y++;
-                    return y;
-                } else {
-                    console.log ("You lose! PAPER beats ROCK")
-                    n++;
-                    return  n;
-                }
-            } 
-            else if (playerSelection === "PAPER") {
-                if (computerSelection === "ROCK"){
-                    console.log ("You win! PAPER beats ROCK")
-                    y++;
-                    return y;
-
-                } else {
-                    console.log ("You lose! CISSORS beat PAPER")
-                    n++;
-                    return n;
-                }
-            } 
-            else if (playerSelection === "SCISSORS") {
-                if (computerSelection === "PAPER"){
-                    console.log ("You win! SCISSORS beat PAPER")
-                    y++;
-                    return y;
-
-                } else {
-                    console.log ("You lose! ROCK beats SCISSORS")
-                    n++;
-                    return n;
-                }
-            } 
-            else {
-                console.log ("Your selection is not valid. Try again.")
-                count-- ;
+            if (playerSelection === computerSelection) {
+                console.log("It is a tie!");                
             }
-    
+
+            else if (playerSelection === "ROCK") {
+                if (computerSelection === "SCISSORS") {
+                    console.log("You win! ROCK beats SCISSORS")
+                    winRound++;
+                    return winRound;
+                } else {
+                    console.log("You lose! PAPER beats ROCK")
+                    loseRound++;
+                    return loseRound;
+                }
+            }
+            else if (playerSelection === "PAPER") {
+                if (computerSelection === "ROCK") {
+                    console.log("You win! PAPER beats ROCK")
+                    winRound++;
+                    return winRound;
+
+                } else {
+                    console.log("You lose! SCISSORS beat PAPER")
+                    loseRound++;
+                    return loseRound;
+                }
+            }
+            else if (playerSelection === "SCISSORS") {
+                if (computerSelection === "PAPER") {
+                    console.log("You win! SCISSORS beat PAPER")
+                    winRound++;
+                    return winRound;
+
+                } else {
+                    console.log("You lose! ROCK beats SCISSORS")
+                    loseRound++;
+                    return loseRound;
+                }
+            }
+            else {
+                console.log("Your selection is not valid. Try again.")
+                roundCount--;
+            }
+
         }
-    
+
         playRound();
 
-    
-
-        
-    } 
-    console.log ("You Won " + y, "You lost " + n);
-
-    if (y > n) {
-        console.log ( "CONGRATULATIONS! YOU ARE THE WINNER!")
-    }
-    else if (y < n) {
-        console.log ( "SORRY! YOU LOSE THE GAME!")
-    }
-    else  console.log ( "THIS GAME IS A TIE!")
 
 
 
     }
-    
-    playGame();
+    console.log("You Won " + winRound, "You lost " + loseRound);
+
+    if (winRound > loseRound) {
+        console.log("CONGRATULATIONS! YOU ARE THE WINNER!")
+    }
+    else if (winRound < loseRound) {
+        console.log("SORRY! YOU LOSE THE GAME!")
+    }
+    else console.log("THIS GAME IS A TIE!")
+
+
+
+}
+
+playGame();
 
 
 
